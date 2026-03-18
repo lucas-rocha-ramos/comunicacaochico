57da34bb5e803becd7448cd3a6b7cb737d77a2fd
Lucas Rocha <designer.br.vG@gmail.com>
2026-03-18 19:01:26 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+```javascript
+const CACHE_NAME = 'agenda-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instala o Service Worker e guarda os arquivos básicos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Responde com o cache quando estiver offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request);
+      })
+  );
+});
